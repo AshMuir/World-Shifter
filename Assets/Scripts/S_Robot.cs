cc33909522c02b8f412fd6d35559876366245d8a
Ashley Muir <ashleygw2@gmail.com>
2015-07-13 23:43:13 +1000

    Started working on movement
    want to make te movement effecient without using Input.GetKey

---
+++ Assets/Scripts/S_Robot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_Chase : MonoBehaviour {
+	public Vector2 move;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InputChecks()
+	{
+		
+	}
+}
